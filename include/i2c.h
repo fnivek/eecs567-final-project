39f2670381d12e1eabaa47fa6a81c6abd82a43d5
Kevin French <fnivek@gmail.com>
2017-03-20 23:44:30 -0400

    I2C: Add timeouts and stop on ack fail

--- include/i2c.h
+++ include/i2c.h
@@ -4,39 +4,53 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/i2c.h>
+#include "systick_handler.h"
 
 #define SCL_PIN GPIO8
 #define SDA_PIN GPIO7
+#define DEFAULT_TIMEOUT 10	//Mills
+
+typedef enum I2CStatus
+{
+	I2C_OK,
+	I2C_TIMEOUT,
+	I2C_BUSY,
+	I2C_ERROR,
+	I2C_ACK_FAIL,
+	I2C_NOT_IMPLEMENTED
+} I2CStatus;
 
 void SetupI2C(void);
 
-void ReadRegI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
+I2CStatus ReadRegI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			uint8_t *buf);
 
-void WriteRegI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
+I2CStatus WriteRegI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			uint8_t *buf);
 
-void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
+I2CStatus ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			uint8_t *buf);
 
-void WriteRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
+I2CStatus WriteRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			uint8_t *buf);
 
 
-void ReadI2C(uint8_t i2c_addr, uint8_t size,
+I2CStatus ReadI2C(uint8_t i2c_addr, uint8_t size,
 			uint8_t *buf);
 
-void WriteI2C(uint8_t i2c_addr, uint8_t size,
+I2CStatus WriteI2C(uint8_t i2c_addr, uint8_t size,
 			uint8_t *buf);
 
-void ReadBlockingI2C(uint8_t i2c_addr, uint8_t size,
+I2CStatus ReadBlockingI2C(uint8_t i2c_addr, uint8_t size,
 			uint8_t *buf);
 
-void WriteBlockingI2C(uint8_t i2c_addr, uint8_t size,
+I2CStatus WriteBlockingI2C(uint8_t i2c_addr, uint8_t size,
 			uint8_t *buf);
-
-uint8_t CheckBusyFlag(void);
 
 void ClearAddrFlag(void);
+
+I2CStatus WaitForFlag(volatile uint32_t* reg, uint32_t flag, uint32_t set_clear, uint32_t timeout);
+
+I2CStatus WaitForAddr(uint32_t timeout);
 
 #endif
--- src/i2c.c
+++ src/i2c.c
@@ -27,9 +27,54 @@ void SetupI2C(void) {
 	i2c_peripheral_enable(I2C1);
 }
 
-// Check the busy flag
-uint8_t CheckBusyFlag(void) {
-	return I2C_SR2(I2C1) & I2C_SR2_BUSY;
+// set_clear 1 to wait until set
+// set_clear 0 to wait till cleared
+// Wait for flag will hang forever if called in the systick callback
+//	because it uses system tick
+I2CStatus WaitForFlag(volatile uint32_t* reg, uint32_t flag,
+	uint32_t set_clear, uint32_t timeout) {
+	uint32_t start_time = Now();
+	while(true) {
+		// Get the value of the flag and set compare to 1 if set
+		//		0 otherwise
+		uint8_t compare = 0;
+		if((*reg) & flag) {
+			compare = 1;
+		}
+
+		// Check if compare is equal to set_clear
+		if(compare == set_clear) {
+			return I2C_OK;
+		}
+
+		// Check if reached timeout
+		if(Now() - start_time > timeout) {
+			return I2C_TIMEOUT;
+		}
+	}
+	return I2C_OK;
+}
+
+I2CStatus WaitForAddr(uint32_t timeout) {
+	uint32_t start_time = Now();
+	// Wait for address
+	while(!(I2C_SR1(I2C1) & I2C_SR1_ADDR)) {
+		// Check if the ack failed
+		if(I2C_SR1(I2C1) & I2C_SR1_AF) {
+			// Stop
+			i2c_send_stop(I2C1);
+
+			// Clear AF
+			I2C_SR1(I2C1) &= ~I2C_SR1_AF;
+			return I2C_ACK_FAIL;
+		}
+
+		// Check for timeout
+		if(Now() - start_time > timeout) {
+			return I2C_TIMEOUT;
+		}
+	}
+	return I2C_OK;
 }
 
 void ClearAddrFlag(void) {
@@ -38,24 +83,29 @@ void ClearAddrFlag(void) {
 	temp = I2C_SR2(I2C1);
 }
 
-void ReadRegI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
+I2CStatus ReadRegI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			uint8_t *buf) {
-
+	return I2C_NOT_IMPLEMENTED;
 }
 
-void WriteRegI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
+I2CStatus WriteRegI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			uint8_t *buf) {
-
+	return I2C_NOT_IMPLEMENTED;
 }
 
 /**
  *	Reads an amount of data from a register on a device
  *	The data is writen to buf
 **/
-void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
+I2CStatus ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			uint8_t *buf) {
+	I2CStatus status;
+
 	// Wait for busy flag to be cleared
-	while(CheckBusyFlag());
+	if(WaitForFlag(&I2C_SR2(I2C1), I2C_SR2_BUSY,
+		0, DEFAULT_TIMEOUT) != I2C_OK) {
+		return I2C_BUSY;
+	}
 
 	// Disable pos
 	I2C_CR1(I2C1) &= ~I2C_CR1_POS;
@@ -66,28 +116,46 @@ void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 	// 	Send start and enter master mode
 	i2c_send_start(I2C1);
 	//	Wait until SB flag set
-	while(!(I2C_SR1(I2C1) & I2C_SR1_SB));
+	if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_SB,
+		1, DEFAULT_TIMEOUT)) {
+		return I2C_TIMEOUT;
+	}
 	//	Send the slave address
 	i2c_send_7bit_address(I2C1, i2c_addr, I2C_READ);
 	//	Wait for addr to be sent
-	while(!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
+	status = WaitForAddr(DEFAULT_TIMEOUT);
+	if(status != I2C_OK) {
+		return status;
+	}
 	//	Clear addr bit
 	ClearAddrFlag();
 	//	Wait for TXE
-	while(!(I2C_SR1(I2C1) & I2C_SR1_TxE));
+	if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_TxE,
+		1, DEFAULT_TIMEOUT)) {
+		return I2C_TIMEOUT;
+	}
 	//	Send register address
 	i2c_send_data(I2C1, reg);
 	// 	Wait for TXE
-	while(!(I2C_SR1(I2C1) & I2C_SR1_TxE));
+	if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_TxE,
+		1, DEFAULT_TIMEOUT)) {
+		return I2C_TIMEOUT;
+	}
 
 	// Generate a restart and write the address of the device again
 	i2c_send_start(I2C1);
 	//	Wait until SB flag set
-	while(!(I2C_SR1(I2C1) & I2C_SR1_SB));
+	if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_SB,
+		1, DEFAULT_TIMEOUT)) {
+		return I2C_TIMEOUT;
+	}
 	//	Send the slave address
 	i2c_send_7bit_address(I2C1, i2c_addr, I2C_READ);
 	//	Wait for addr to be sent
-	while(!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
+	status = WaitForAddr(DEFAULT_TIMEOUT);
+	if(status != I2C_OK) {
+		return status;
+	}
 
 	// Read the data!
 	if(size == 1) {
@@ -113,7 +181,10 @@ void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			// One byte left
 			if(size == 1) {
 				// Wait for RXNE
-				while(!(I2C_SR1(I2C1) & I2C_SR1_RxNE));
+				if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_RxNE,
+					1, DEFAULT_TIMEOUT)) {
+					return I2C_TIMEOUT;
+				}
 
 				// Copy data to buf
 				(*buf++) = i2c_get_data(I2C1);
@@ -121,7 +192,10 @@ void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			// Two bytes left
 			} else if(size == 2) {
 				// Wait for BTF flag
-				while(!(I2C_SR1(I2C1) & I2C_SR1_BTF));
+				if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_BTF,
+					1, DEFAULT_TIMEOUT)) {
+					return I2C_TIMEOUT;
+				}
 
 				// Send stop
 				i2c_send_stop(I2C1);
@@ -134,7 +208,10 @@ void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			// Last 3 bytes
 			} else {
 				// Wait for BTF flag
-				while(!(I2C_SR1(I2C1) & I2C_SR1_BTF));
+				if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_BTF,
+					1, DEFAULT_TIMEOUT)) {
+					return I2C_TIMEOUT;
+				}
 
 				// Disable ack
 				i2c_disable_ack(I2C1);
@@ -144,7 +221,10 @@ void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 				--size;
 
 				// Wait for BTF flag
-				while(!(I2C_SR1(I2C1) & I2C_SR1_BTF));
+				if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_BTF,
+					1, DEFAULT_TIMEOUT)) {
+					return I2C_TIMEOUT;
+				}
 
 				// Send stop
 				i2c_send_stop(I2C1);
@@ -158,7 +238,10 @@ void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 		// More than 3 bytes
 		} else {
 			// Wait for RXNE
-			while(!(I2C_SR1(I2C1) & I2C_SR1_RxNE));
+			if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_RxNE,
+				1, DEFAULT_TIMEOUT)) {
+				return I2C_TIMEOUT;
+			}
 
 			// Copy data to buf
 			(*buf++) = i2c_get_data(I2C1);
@@ -172,12 +255,17 @@ void ReadRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			}
 		}
 	}
+	return I2C_OK;
 }
 
-void WriteRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
+I2CStatus WriteRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			uint8_t *buf) {
 	// Wait for busy flag to be cleared
-	while(CheckBusyFlag());
+	// Wait for busy flag to be cleared
+	if(WaitForFlag(&I2C_SR2(I2C1), I2C_SR2_BUSY,
+		0, DEFAULT_TIMEOUT) != I2C_OK) {
+		return I2C_BUSY;
+	}
 
 	// Disable pos
 	I2C_CR1(I2C1) &= ~I2C_CR1_POS;
@@ -186,22 +274,34 @@ void WriteRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 	//	Send start
 	i2c_send_start(I2C1);
 	//	Wait until SB flag set
-	while(!(I2C_SR1(I2C1) & I2C_SR1_SB));
+	if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_SB, 1,
+		DEFAULT_TIMEOUT)) {
+		return I2C_TIMEOUT;
+	}
 	//	Send addr
 	i2c_send_7bit_address(I2C1, i2c_addr, I2C_WRITE);
 	//	Wait for addr to be sent
-	while(!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
+	I2CStatus status = WaitForAddr(DEFAULT_TIMEOUT);
+	if(status != I2C_OK) {
+		return status;
+	}
 	//	Clear addr bit
 	ClearAddrFlag();
 	//	Wait for TXE
-	while(!(I2C_SR1(I2C1) & I2C_SR1_TxE));
+	if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_TxE, 1,
+		DEFAULT_TIMEOUT)) {
+		return I2C_TIMEOUT;
+	}
 	//	Send register address
 	i2c_send_data(I2C1, reg);
 
 	// Send the data
 	while(size > 0) {
 		// 	Wait for TXE
-		while(!(I2C_SR1(I2C1) & I2C_SR1_TxE));
+		if(WaitForFlag(&I2C_SR1(I2C1), I2C_SR1_TxE, 1,
+			DEFAULT_TIMEOUT)) {
+			return I2C_TIMEOUT;
+		}
 
 		// Write data
 		i2c_send_data(I2C1, (*buf++));
@@ -214,27 +314,26 @@ void WriteRegBlockingI2C(uint8_t i2c_addr, uint8_t reg, uint8_t size,
 			--size;
 		}
 	}
-
-
+	return I2C_OK;
 }
 
 
-void ReadI2C(uint8_t i2c_addr, uint8_t size,
+I2CStatus ReadI2C(uint8_t i2c_addr, uint8_t size,
 			uint8_t *buf) {
-
+	return I2C_NOT_IMPLEMENTED;
 }
 
-void WriteI2C(uint8_t i2c_addr, uint8_t size,
+I2CStatus WriteI2C(uint8_t i2c_addr, uint8_t size,
 			uint8_t *buf) {
-
+	return I2C_NOT_IMPLEMENTED;
 }
 
-void ReadBlockingI2C(uint8_t i2c_addr, uint8_t size,
+I2CStatus ReadBlockingI2C(uint8_t i2c_addr, uint8_t size,
 			uint8_t *buf) {
-
+	return I2C_NOT_IMPLEMENTED;
 }
 
-void WriteBlockingI2C(uint8_t i2c_addr, uint8_t size,
+I2CStatus WriteBlockingI2C(uint8_t i2c_addr, uint8_t size,
 			uint8_t *buf) {
-
+	return I2C_NOT_IMPLEMENTED;
 }
