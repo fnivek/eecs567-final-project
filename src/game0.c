694ffcf980fe43dfd0f21df1d81cce7e46a439d2
scutlip1 <scutlip1@users.noreply.github.com>
2017-04-05 12:06:35 -0400

    game0.c updated

--- src/game0.c
+++ src/game0.c
@@ -19,11 +19,12 @@ uint8_t ledmat_x;
 uint8_t ledmat_y;
 
 //Motor
-uint_8_t motorx;
-uint_8_t motory;
-uint_8_t motorcoords[2];
-unit_8_t motorcoordsold[2];
-unit_8_t zmotormag;
+uint_8_t mx;
+uint_8_t my;
+uint_8_t jnew[3]; //Motor Coords
+unit_8_t jold[3];
+unit_8_t jsend[3];
+unit_8_t zmag;
 float stepsize = .05;
 
 float l1 = 6;
@@ -46,30 +47,23 @@ int Game0(void){
 		ledmat_y = round(yjdata/ydivs);
 	//Decompose: Joystick to Motor Space
 		if(zposdata == 1){
-			zjdata = zmotormag;
+			zjdata = zmag;
 		}else if(znegdata == 1){
-			zjdata = -1*zmotormag;
+			zjdata = -1*zmag;
 		}else{
 			zjdata = 0;
 		}
-		motorcoords = motorcoords_old + stepsize*[xjdata yjdata zjdata]; //Psuedo xdot ydot zdot
-		motorcoords_old = motorcoords;
+		jnew = jold + stepsize*[xjdata yjdata zjdata]; //Psuedo xdot ydot zdot
+		jold = jnew;
 	//Write: to Screen
 		LedmatDrawPixel(CastPoint2(ledmat_x, ledmat_y), LEDMAT_LED_GREEN);
 		LedmatRefreshDisplay();
 	//Read: Motor
-	//need current motor angles
-		jspace = motorread()
-		th1 = jspace(1);
-		th2 = jspace(2);
-		float th12 = th1 + th2;
+		ArmGetJointAngles([kBase,kShoulder,kElbow],jold,3))
 	//Write: to Motor 
-	//Inverse 2x2 
-	// J = [l1cth1+l2cth12, l2cth12; -l1sth1-l2sth12 -lsth12]
 		Jinv = [sin(th12)/(l1*sin(th2)) cos(th12)/(l1*sin(th2));
 				(-(l2*sin(th12))+l1*sin(th1))/(l1*l2*sin(th2)) (-l1*cos(th12) + l1*cos(th1))/(l1*l2*sin(th2))];
-		motormove = ([Jinv*motorcoords(0:1),motorcoords(2)]);
-		ArmSetJointAngle(0,motormove(0));
-		ArmSetJointAngle(1,motormove(1));
-		ArmSetJointAngle(2,motormove(2));
+		jnew(0) = zjdata
+		jsend = ([jnew(0),Jinv*jnew(1:2)]);
+		ArmSetJointAngles([kBase,kShoulder,kElbow],jsend,3)
 }
